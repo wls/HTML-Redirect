e3bbbb1c3bda66fcea740ce4f86e1b0345f5cc92
Walt Stoneburner <wls@wwco.com>
2025-08-24 13:13:28 -0400

    Modernized Redirect

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
 <?php
-  $redirect_to = "https://www.newlocation.com/"; /* Put your URL here */
-  header('Location: ' . $redirect_to);
+  $redirect_to = "https://www.newlocation.com/";
+  header('Location: ' . $redirect_to, true, 302);
+  exit; // Always call exit after header redirects
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-        <meta content="utf-8" http-equiv="encoding">
-        <meta http-equiv="refresh" content="0; URL='<?= $redirect_to ?>'" />
-        <link rel="canonical" href="<?= $redirect_to ?>"/>
-    </head>
-    <body>
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-
-		<h1>Redirect</h1>
-        <p>The location you're attempting to reach has moved, please click on the new location:</p>
-        <p><a href="<?= $redirect_to ?>"><?= $redirect_to ?></a></p>
-
-        <script>
-          window.location.href = "<?= $redirect_to ?>";
-        </script>
-    </body>
+<head>
+  <meta charset="UTF-8">
+  <title>Redirecting...</title>
+  <meta http-equiv="refresh" content="0; url=<?= htmlspecialchars($redirect_to) ?>">
+  <link rel="canonical" href="<?= htmlspecialchars($redirect_to) ?>" />
+</head>
+<body>
+  <p>If you are not redirected automatically, follow this <a href="<?= htmlspecialchars($redirect_to) ?>">link</a>.</p>
+</body>
 </html>
